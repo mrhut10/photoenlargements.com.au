0b14bcad72f316be498191f99eee56cb4abf4cf5
Luke Bennett <hello@lukebennett.com.au>
2020-01-11 21:32:35 +1100

    Add Banner section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Why from '../components/why-print-with-us';
+import Banner from '../components/banner';
 
 const IndexPage = () => (
   <Layout home>
     <SEO title="Photo Enlargements | Scrapbooks | Canvas" />
     <Why />
+    <Banner>We offer a variety of enlargement sizes</Banner>
   </Layout>
 );
 
