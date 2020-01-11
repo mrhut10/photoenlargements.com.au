f73bbda56b6ee6e0dd4590621eca7d3c8a4b642d
Luke Bennett <hello@lukebennett.com.au>
2020-01-11 22:12:25 +1100

    Add about section

---
+++ src/components/about.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'gatsby-image';
+
+import useGraphqlQueries from './use-graphql-queries';
+
+const About = () => {
+  const data = useGraphqlQueries();
+  return (
+    <article id="about" className="px-10 mt-12 leading-loose">
+      <div className="flex flex-wrap max-w-6xl mx-auto">
+        <div className="w-full md:w-1/2">
+          <Image fluid={data.heroImage.childImageSharp.fluid} />
+        </div>
+        <div className="w-full md:w-1/2 md:pl-20">
+          <h2 className="mt-6 text-5xl font-bold text-gold font-display">
+            About
+          </h2>
+          <p>
+            We create beautiful, lasting memories with our photography services,
+            with years of experience in all verses of photography, we will do
+            our best at capturing the best images to set the scene!
+          </p>
+          <p className="mt-4">
+            Contact our friendly team or come and see our studio at 21 Boundary
+            Street Glenreagh NSW 2450!
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,14 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Why from '../components/why-print-with-us';
 import Banner from '../components/banner';
+import About from '../components/about';
 
 const IndexPage = () => (
   <Layout home>
     <SEO title="Photo Enlargements | Scrapbooks | Canvas" />
     <Why />
     <Banner>We offer a variety of enlargement sizes</Banner>
+    <About />
   </Layout>
 );
 
