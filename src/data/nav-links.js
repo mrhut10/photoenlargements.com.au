72dde3e821f095d8d4108f004ba3d423666d6e8d
Luke Bennett <hello@lukebennett.com.au>
2020-01-13 08:07:05 +1100

    Change slug

--- src/data/nav-links.js
+++ src/data/nav-links.js
@@ -1,7 +1,7 @@
 const navLinks = [
   { title: 'Home', route: '/' },
   { title: 'Services', route: '/services' },
-  { title: 'Shop', route: '/shop' },
+  { title: 'Shop', route: '/products' },
   { title: 'Price Chart', route: '/price-chart' },
   { title: 'Gallery', route: '/gallery' },
   { title: 'Contact Us', route: '/contact-us' },
